4e6c027a5830a45b399802528e1332d640a2a2a9
Maeve Lander <4695433+maevelander@users.noreply.github.com>
2018-05-08 17:11:43 +0800

    link ui text

--- inc/admin/class-ramp-for-gutenberg-post-type-settings-ui.php
+++ inc/admin/class-ramp-for-gutenberg-post-type-settings-ui.php
@@ -128,8 +128,7 @@ class Ramp_For_Gutenberg_Post_Type_Settings_UI {
 							</label>
 							<?php if ( $is_helper_enabled_post_type ): ?>
 								<small style="margin-left: 1rem;">
-									<?php // @TODO: Insert URL Here ?>
-									<a href="#"><?php esc_html_e( 'Why is this disabled?', 'ramp-for-gutenberg' ); ?></a>
+									<a href="https://github.com/Automattic/ramp-for-gutenberg#faqs"><?php esc_html_e( 'Why is this disabled?', 'ramp-for-gutenberg' ); ?></a>
 								</small>
 							<?php endif; ?>
 							<br>
@@ -147,8 +146,7 @@ class Ramp_For_Gutenberg_Post_Type_Settings_UI {
 					esc_html__( 'For more granular control you can use the %s function.', 'ramp-for-gutenberg' ),
 					'<code>ramp_for_gutenberg_load_gutenberg()</code>'
 				); ?>
-				<?php // @TODO: Insert URL Here ?>
-				<a href="#" target="_blank"><?php esc_html_e( 'Learn more', 'ramp-for-gutenberg' ); ?></a>
+				<a href="https://github.com/Automattic/ramp-for-gutenberg#faqs" target="_blank"><?php esc_html_e( 'Learn more', 'ramp-for-gutenberg' ); ?></a>
 			</p>
 		</div>
 		<?php
